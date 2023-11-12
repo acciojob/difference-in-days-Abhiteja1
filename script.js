a70ba28cc28004e2d8d8f8ebd96fc03f113f7949
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-12 18:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const diff= Number(date1.slice(9,11))-Number(date2.slice(9,11));
+	return diff;
 };
 
 // Do not change the code below.
